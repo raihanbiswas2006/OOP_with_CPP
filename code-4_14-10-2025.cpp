300c9ed33e6d625aeb72c13e92579908e7564f3d
Raihan Biswas <raihanbiswas2006@gmail.com>
2025-10-30 00:48:01 +0600

    Swaping

---
+++ code-4_14-10-2025.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class swaping{
+    public:
+    int a, b, temp;
+
+    void swp();
+};
+
+void swaping::swp(){
+    
+    cin >> a >> b;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+        cout << "SWAP: \n" << a << "\n" << b <<endl;
+}
+
+int main(){
+    swaping a;
+    a.swp();
+    return 0;
+}
