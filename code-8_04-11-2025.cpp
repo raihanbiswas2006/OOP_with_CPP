f99fb17dd6724414df8ae51084a5a57602c70f9f
Raihan Biswas <raihanbiswas2006@gmail.com>
2025-11-14 23:51:16 +0600

    Average Multiple

---
+++ code-8_04-11-2025.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+float average(int a, int b)
+{
+    return (a + b) / 2.0f;
+}
+
+float average(float a, int b)
+{
+    return (a + b) / 2.0f;
+}
+
+float average(int a, float b)
+{
+    return (a + b) / 2.0f;
+}
+
+int main()
+{
+    float a, b, avg;
+
+    cout << "Give First Number: ";
+    cin >> a;
+
+    cout << "Give Second Number: ";
+    cin >> b;
+
+    avg = average((int)a, (int)b);
+    cout << "Average (int, int): " << avg <<endl;
+
+    avg = average(a, (int)b);
+    cout << "Average (float, int): " << avg <<endl;
+
+    avg = average((int)a, b);
+    cout << "Average (int, float): " << avg <<endl;
+
+    return 0;
+}
