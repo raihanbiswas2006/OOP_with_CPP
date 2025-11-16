fab57b16166a2fcbc59d802bd9364ef8998d7f2d
Raihan Biswas <raihanbiswas2006@gmail.com>
2025-11-16 23:41:14 +0600

    Calculator Simple

---
+++ code-09_11-11-2025.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+inline int add(int a, int b){
+    return (a+b);
+}
+inline int sub(int a, int b){
+    return (a-b);
+}
+inline int multi(int a, int b){
+    return (a*b);
+}
+inline float div(float p, float q){
+    return (p/q);
+}
+
+int main(){
+    float x, y;
+
+    cout << "\n Give Two Numbers: ";
+    cin >> x >> y;
+
+    cout << "Sumation of two Numbers: " << add(x,y);
+    cout << "\n Subtraction of two Numbers: " << sub(x,y);
+    cout << "\n Multiplication of two Numbers: " << multi(x,y);
+    cout << "\n Divition of two Numbers: " << div(x,y);
+
+    return 0;
+}
