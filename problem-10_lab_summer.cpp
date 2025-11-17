3197f5b092a3c2e2dbf0db501c901772f555b7e4
Raihan Biswas <raihanbiswas2006@gmail.com>
2025-11-18 03:09:51 +0600

    Problem 10

---
+++ problem-10_lab_summer.cpp
@@ -0,0 +1,36 @@
+// Problem 10
+
+#include <iostream>
+using namespace std;
+
+class PrimeNumbers
+{
+public:
+    void printPrimes()
+    {
+        int num, i;
+        
+        for (num = 2; num <= 100; num++)
+        {
+            bool isPrime = true;
+
+            for (i = 2; i * i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+                cout << num << " ";
+        }
+    }
+};
+
+int main()
+{
+    PrimeNumbers p;
+    p.printPrimes();
+}
