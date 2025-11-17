7be70dbef7019e420a6345fc54e25bb0a2242d23
Raihan Biswas <raihanbiswas2006@gmail.com>
2025-11-18 02:17:57 +0600

    Problem 6

---
+++ problem-06_lab_summer.cpp
@@ -0,0 +1,30 @@
+// Problem 6
+
+#include <iostream>
+using namespace std;
+
+int Sum(int a, int b)
+{
+    return a + b;
+}
+
+int Sum(int a, int b, int c)
+{
+    return a + b + c;
+}
+
+int main()
+{
+
+    int a, b, c;
+
+    cout << "Give 2 Numbers: ";
+    cin >> a >> b;
+    cout << "Sum = " << Sum(a, b) << endl;
+
+    cout << "Give 3 Numbers: ";
+    cin >> a >> b >> c;
+    cout << "Sum = " << Sum(a, b, c) << endl;
+
+    return 0;
+}
