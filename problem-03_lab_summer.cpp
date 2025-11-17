9729dffd987f8930014191115f353f16f41aa673
Raihan Biswas <raihanbiswas2006@gmail.com>
2025-11-18 01:34:46 +0600

    Problem 3 A

---
+++ problem-03_lab_summer.cpp
@@ -0,0 +1,33 @@
+// Problem 3 (a)
+
+#include <iostream>
+using namespace std;
+
+class swaping
+{
+public:
+    int a, b, temp;
+
+    void swp();
+};
+
+void swaping::swp()
+{
+
+    cin >> a >> b;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "SWAP: \n"
+         << a << "\n"
+         << b << endl;
+}
+
+int main()
+{
+    swaping a;
+    a.swp();
+    return 0;
+}
